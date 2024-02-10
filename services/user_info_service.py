510a02f239580cf97c232a29f05e71b77df8c2d4
Genisys <17355439+guh0613@users.noreply.github.com>
2024-02-10 13:59:46 +0800

    🛠 refactor: optimize ehall_url check

--- services/user_info_service.py
+++ services/user_info_service.py
@@ -1,14 +1,14 @@
 import requests
 
-from configs.config import EHALL_SERVER_URLS
+from utils.common_utils import get_ehall_url
 from utils.request_utils import default_header
 
 
 def get_user_info(school_name: str, mod_auth_cas: str) -> tuple[dict, int]:
     # check if school is supported
-    if school_name not in EHALL_SERVER_URLS:
+    ehall_url = get_ehall_url(school_name)
+    if ehall_url is None:
         return {'status': 'error', 'message': f'{school_name} is not supported'}, 400
-    ehall_url = EHALL_SERVER_URLS[school_name]
 
     # get user info
     s = requests.Session()
@@ -20,11 +20,11 @@ def get_user_info(school_name: str, mod_auth_cas: str) -> tuple[dict, int]:
 
     # check if the response is valid, and get 'username', 'userid', 'userType', 'userDepartment',  'userSex'
     if response.status_code != 200:
-        return {'status': 'error', 'message': 'Failed to get user info'}, 400
+        return {'status': 'error', 'message': 'Failed to get user info.MOD_AUTH_CAS is probably invalid.'}, 400
     try:
         user_info_orig = response.json()['data']
     except KeyError:
-        return {'status': 'error', 'message': 'Failed to get user info'}, 400
+        return {'status': 'error', 'message': 'Failed to get user info.MOD_AUTH_CAS is probably invalid.'}, 400
     except Exception as e:
         return {'status': 'error', 'message': f'Unknown: {str(e)}'}, 400
     user_info: dict = {'status': 'OK', 'message': 'User info retrieved successfully',
--- utils/common_utils.py
+++ utils/common_utils.py
@@ -4,3 +4,8 @@ from configs import config
 def get_cas_url(school_name: str) -> str:
     """return the CAS URL for the specified school."""
     return config.CAS_SERVER_URLS.get(school_name, None)
+
+
+def get_ehall_url(school_name: str) -> str:
+    """return the eHall URL for the specified school."""
+    return config.EHALL_SERVER_URLS.get(school_name, None)
