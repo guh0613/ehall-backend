aae1f6bc16c0c03664822cf148b42696240d7eae
Genisys <guh0613@icloud.com>
2024-08-07 10:41:48 +0800

    🚀perf: Optimize function performance `random_string` but keep the logic same with nnu
    Signed-off-by: Genisys <guh0613@icloud.com>

--- utils/encryption_utils.py
+++ utils/encryption_utils.py
@@ -1,7 +1,8 @@
+import random
+from base64 import b64encode
+
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad
-from base64 import b64encode
-import random
 
 
 def aes_cbc_encrypt_url(data, key):
@@ -18,5 +19,5 @@ def aes_cbc_encrypt_url(data, key):
 def random_string(length: int):
     """ return a random string with the specified length."""
     chars = 'ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678'
-    return ''.join(random.choice(chars) for _ in range(length))
-
+    char_list = list(chars)
+    return ''.join(random.choices(char_list, k=length))
